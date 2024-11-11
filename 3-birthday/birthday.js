403a04f1474d06c9ddf8af1895045270c4c1b1bb
183Natahsa <183natasha@mail.ru>
2024-11-11 15:06:40 +0000

    конечный вариант

--- 3-birthday/birthday.js
+++ 3-birthday/birthday.js
@@ -5,7 +5,6 @@ birthday = new Date(birthday)
 const now = new Date();
 
 let goodBirthday = new Date((now.getFullYear() - 14), now.getMonth(), (now.getDate() +1))
-console.log(goodBirthday)
 
 function control(now, birthday) {
     if (birthday.getTime() <= goodBirthday.getTime()) {
