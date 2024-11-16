83bfb527f4a045fe1edfdbf7f5ea242ecb8c16fd
183Natasha <168049595+183Natasha@users.noreply.github.com>
2024-11-16 15:16:53 +0500

    создан класс Person

--- 5-oop/oop.js
+++ 5-oop/oop.js
@@ -1,2 +1,20 @@
 'use strict'
 
+function Person(race, name, language) {
+    this.race = race;     // Свойство расса
+    this.name = name;   // Свойство имя
+    this.language = language;   // Свойство язык
+}
+
+Person.prototype.speak = function(){
+    console.log(`Меня зовут ${this.name}, мой родной язык - ${this.language}`)
+}
+
+const orc = new Person('орк', 'Pppг','оркский');
+
+orc.speak()
+
+
+
+// const orc = new Orс( 'орк', 'Pppг','оркский');
+// const elf = new Elf('эльф', 'Элланиэль', 'эльфийский' );
