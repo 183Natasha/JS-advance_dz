2a9d2ea6145efa8815c3bc32b694caaaaac853e5
183Natasha <168049595+183Natasha@users.noreply.github.com>
2024-12-17 22:58:28 +0500

    вывод информации о покемоне на английском

--- 10-pokemon-promis/pokemon-promis.js
+++ 10-pokemon-promis/pokemon-promis.js
@@ -5,5 +5,13 @@ fetch('https://pokeapi.co/api/v2/pokemon/ditto')
     .then(({abilities}) => {
         return fetch(abilities[0].ability.url)
     })
+    .then(response => response.json())
+    .then(({ effect_entries})=>{
+        for (let el in effect_entries ) {
+            if (effect_entries [el].language.name === 'en') {
+                console.log(effect_entries [el].effect)
+            }
+        }
+    })
     
     .catch(()=>console.log("Произошла ошибка"))
