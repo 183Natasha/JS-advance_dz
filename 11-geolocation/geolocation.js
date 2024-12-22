e653294aa7a36e8d3b388ed8207a5d5b4014a247
183Natasha <168049595+183Natasha@users.noreply.github.com>
2024-12-22 17:34:39 +0500

    ошибки обработаны

--- 11-geolocation/geolocation.js
+++ 11-geolocation/geolocation.js
@@ -1,17 +1,25 @@
 'use strict'
 
 function userPosition() {
-    return new Promise((resolve) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const latitudeUser = position.coords.latitude;
-            const longitudeUser = position.coords.longitude;
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+                position => {
+                    console.log('Доступ к геолокации предоставлен, координаты получены:'); 
+                    const latitudeUser = position.coords.latitude;
+                    const longitudeUser = position.coords.longitude;
+        
+                    resolve(`Широта - ${latitudeUser} Долгота - ${longitudeUser}`)}
+                
+                ,
+                
+                error => {
+                    reject(new Error ('Доступ к геолокации отклонён.') )   
+                }
+    )}
+)}
 
-            resolve(`Широта - ${latitudeUser} Долгота - ${longitudeUser}`)
-        })
-    })
-}
 
 userPosition()
     .then((data) => console.log(data))
-    .catch((error) => console.error(`Упс, произошла ошибка. Error: ${error.message})`))
-    .finally(() =>console.log('Пользователь разрешил доступ к геопозиции'))
+    .catch((error) => console.error(`Упс, произошла ошибка. Error: ${error.message}`))
+    .finally(() =>console.log('Программа работает'))
