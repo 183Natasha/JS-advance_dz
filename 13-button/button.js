3f7418548ccf54443c5133b6c50ff5b067eee9f1
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-01-19 19:56:50 +0500

    коррекция

--- 13-button/button.js
+++ 13-button/button.js
@@ -1,3 +1,22 @@
 'use strict'
 
-let counter = 0;
+const buttons = document.querySelector('.buttons')
+const clicks = document.querySelector('.clicks')
+const clicks__counter = document.querySelector('.clicks__counter')
+
+let counter = 0
+
+buttons.addEventListener('click', (event) => {
+    if (event.target.className === 'button') { 
+        counter += 1
+        clicks__counter.innerHTML = counter
+
+        for (let el of [...buttons.children]){
+            el.innerHTML = 'Нажми меня';
+
+            event.target.innerHTML = "Нажата!";
+        }
+        
+    }
+    
+})
